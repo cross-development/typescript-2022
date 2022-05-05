33979c60c04f390b5547ee2585f2132a9bea1d16
Vitaliy Derda <derdavitaliy@gmail.com>
2022-05-05 13:23:48 +0300

    feat: added tuple types

---
+++ src/tuple_types.ts
@@ -0,0 +1,9 @@
+{
+  const skills: [number, string] = [1, "Dev"];
+  const id = skills[0];
+  const name = skills[1];
+
+  const [skillId, skillName] = skills;
+
+  const arr: [number, string, ...boolean[]] = [1, "Dev", true, true];
+}
