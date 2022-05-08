a13dc0b9611275765242427593efa2e40dc2ddb2
Vitaliy Derda <derdavitaliy@gmail.com>
2022-05-08 13:57:27 +0300

    feat: added null type

---
+++ src/advanced_types/null.ts
@@ -0,0 +1,27 @@
+const n: null = null;
+// const n: null = undefined  // error
+const n1: any = null;
+// const n2: number = null; // error
+// const n3: string = null; // error
+// const n4: boolean = null; // error
+// const n5: undefined = null; // error
+
+interface User {
+  name: string;
+}
+
+function getUser() {
+  if (Math.random() > 0.5) {
+    return null;
+  } else {
+    return {
+      name: "Vasia",
+    } as User;
+  }
+}
+
+const user = getUser();
+
+if (user) {
+  console.log("user.name", user.name);
+}
