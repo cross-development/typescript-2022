c6b17068dee9cc7a59e11c43cdfb8db0e300657e
Vitaliy Derda <derdavitaliy@gmail.com>
2022-05-09 10:24:52 +0300

    feat: added assert type

---
+++ src/advanced_types/asserts.ts
@@ -0,0 +1,18 @@
+{
+  interface User {
+    name: string;
+  }
+
+  const a = {};
+
+  assertUser(a);
+  a.name;
+
+  function assertUser(obj: unknown): asserts obj is User {
+    if (typeof obj === "object" && !!obj && "name" in obj) {
+      return;
+    }
+
+    throw new Error("Is not user");
+  }
+}
