93ffd0b1bf1bcf071d963843945ef60c5f957687
Vitalii Derda <vderda@s-pro.io>
2022-06-10 19:49:25 +0300

    feat: added typeof operator

---
+++ src/type_manipulation/typeof.ts
@@ -0,0 +1,34 @@
+{
+  let strOrNum: string | number = 5;
+
+  // type of strOrNum = number
+  //   if (strOrNum) {
+  //   }
+
+  if (Math.random() > 0.5) {
+    strOrNum = 5;
+  } else {
+    strOrNum = "str";
+  }
+
+  if (typeof strOrNum === "string") {
+    console.log("strOrNum", strOrNum);
+  } else {
+    console.log("strOrNum", strOrNum);
+  }
+
+  let str2OrNum: typeof strOrNum;
+
+  const user = {
+    name: "John",
+  };
+
+  type keyOfUser = keyof typeof user;
+
+  enum Direction {
+    Up,
+    Down,
+  }
+
+  type D = keyof typeof Direction;
+}
