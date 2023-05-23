381f0d6edd826b7ca33ed70746b99e75f012e5b4
Vitaliy Derda <derdavitaliy@gmail.com>
2023-05-23 18:53:20 +0300

    feat: added params decorator with reflect-metadata

--- src/decorators/paramsDecorator.ts
+++ src/decorators/paramsDecorator.ts
@@ -1,3 +1,7 @@
+import 'reflect-metadata';
+
+const POSITIVE_METADATA_KEY = Symbol('POSITIVE_METADATA_KEY');
+
 {
   interface IUserService {
     users: number;
@@ -12,24 +16,49 @@
       return this.users;
     }
 
+    @Validate()
     setUsersInDatabase(@Positive() num: number): void {
       this.users = num;
     }
   }
 
   function Positive() {
+    return (target: Object, propertyKey: string | symbol, paramIndex: number) => {
+      console.log(Reflect.getOwnMetadata('design:type', target, propertyKey));
+      console.log(Reflect.getOwnMetadata('design:paramtypes', target, propertyKey));
+      console.log(Reflect.getOwnMetadata('design:returntype', target, propertyKey));
+
+      let existsParams: number[] = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey) || [];
+
+      existsParams.push(paramIndex);
+
+      Reflect.defineMetadata(POSITIVE_METADATA_KEY, existsParams, target, propertyKey);
+    };
+  }
+
+  function Validate() {
     return (
       target: Object,
       propertyKey: string | symbol,
-      paramIndex: number,
+      descriptor: TypedPropertyDescriptor<(...args: any[]) => any>,
     ) => {
-      console.log('target', target); // {}
-      console.log('propertyKey', propertyKey); // setUsersInDatabase
-      console.log('paramIndex', paramIndex); // 0 - param position
+      let method = descriptor.value;
+      descriptor.value = function (...args: any) {
+        let positiveParams: number[] = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, propertyKey);
+
+        if (positiveParams) {
+          for (const index of positiveParams) {
+            if (args[index] < 0) {
+              throw new Error('Must be higher then 0');
+            }
+          }
+        }
+
+        return method?.apply(this, args);
+      };
     };
   }
 
   const userService = new UserService();
-  userService.users = 1;
-  console.log('userService.users', userService.users); // 1
+  console.log('userService', userService);
 }
