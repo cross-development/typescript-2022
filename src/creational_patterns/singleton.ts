9539a7a064d2c561f0b289cab2befc0dc73bec12
Vitaliy Derda <derdavitaliy@gmail.com>
2023-05-25 17:51:49 +0300

    feat: added creatrional pattern - singleton

---
+++ src/creational_patterns/singleton.ts
@@ -0,0 +1,42 @@
+{
+  class MyMap {
+    private static instance: MyMap;
+
+    map: Map<number, string> = new Map<number, string>();
+
+    private constructor() {}
+
+    clean() {
+      this.map = new Map<number, string>();
+    }
+
+    public static get(): MyMap {
+      if (!MyMap.instance) {
+        MyMap.instance = new MyMap();
+      }
+
+      return MyMap.instance;
+    }
+  }
+
+  class Service1 {
+    addMap(key: number, value: string) {
+      const myMap = MyMap.get();
+
+      myMap.map.set(key, value);
+    }
+  }
+
+  class Service2 {
+    getKeys(key: number) {
+      const myMap = MyMap.get();
+
+      console.log(myMap.map.get(key));
+      myMap.clean();
+      console.log(myMap.map.get(key));
+    }
+  }
+
+  new Service1().addMap(1, 'Works');
+  new Service2().getKeys(1);
+}
