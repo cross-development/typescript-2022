3b2fbd656a262e0315a4cf3e7162f2a71edd9127
Vitaliy Derda <derdavitaliy@gmail.com>
2023-06-01 09:26:38 +0300

    feat: added behavior pattern - iterator

---
+++ src/behavioral_patterns/iterator.ts
@@ -0,0 +1,84 @@
+{
+  class Task {
+    constructor(public priority: number) {}
+  }
+
+  class TaskList {
+    private tasks: Task[] = [];
+
+    public sortByPriority() {
+      this.tasks = this.tasks.sort((a, b) => {
+        if (a.priority > b.priority) {
+          return 1;
+        } else if (a.priority == b.priority) {
+          return 0;
+        } else {
+          return -1;
+        }
+      });
+    }
+
+    public addTask(task: Task) {
+      this.tasks.push(task);
+    }
+
+    public getTasks() {
+      return this.tasks;
+    }
+
+    public countTasks() {
+      return this.tasks.length;
+    }
+
+    public getIterator() {
+      return new PriorityTaskIterator(this);
+    }
+  }
+
+  interface IIterator<T> {
+    index(): number;
+    next(): T | undefined;
+    prev(): T | undefined;
+    current(): T | undefined;
+  }
+
+  class PriorityTaskIterator implements IIterator<Task> {
+    private position: number = 0;
+    private taskList: TaskList;
+
+    constructor(taskList: TaskList) {
+      taskList.sortByPriority();
+      this.taskList = taskList;
+    }
+
+    index(): number {
+      return this.position;
+    }
+
+    next(): Task | undefined {
+      this.position += 1;
+      return this.taskList.getTasks()[this.position];
+    }
+
+    prev(): Task | undefined {
+      this.position -= 1;
+      return this.taskList.getTasks()[this.position];
+    }
+
+    current(): Task | undefined {
+      return this.taskList.getTasks()[this.position];
+    }
+  }
+
+  const taskList = new TaskList();
+  taskList.addTask(new Task(8));
+  taskList.addTask(new Task(1));
+  taskList.addTask(new Task(3));
+
+  const iterator = taskList.getIterator();
+  iterator.current(); // 1
+  iterator.next(); // 3
+  iterator.next(); // 8
+  iterator.prev(); // 3
+  iterator.index(); // 1
+}
